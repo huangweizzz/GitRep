0ae2b164f4458d36946198ec5d09cefaeca06cee
huangweizzz <631889590@qq.com>
2021-02-01 23:48:10 +0800

    springcloud学习-ribbon告结

--- SpringCloud/springcloud-cunsumer-dept-80/src/main/java/com/springcloud/study/DeptConsumer_80.java
+++ SpringCloud/springcloud-cunsumer-dept-80/src/main/java/com/springcloud/study/DeptConsumer_80.java
@@ -1,12 +1,17 @@
 package com.springcloud.study;
 
+import com.springcloud.myrule.EgoRule;
+import com.springcloud.myrule.MyRandomRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 //ribbon和eureka整合以后,客户端可以直接调用,不用关系ip地址和端口号
 @SpringBootApplication
 @EnableEurekaClient
+//在微服务启动的时候,能够加载我们自定义Ribbon类 name 微服务名称
+@RibbonClient (name = "springcloud-provider-dept",configuration = EgoRule.class)
 public class DeptConsumer_80 {
     public static void main(String[] args) {
         SpringApplication.run(DeptConsumer_80.class,args);
--- SpringCloud/springcloud-cunsumer-dept-80/src/main/java/com/springcloud/study/config/configBean.java
+++ SpringCloud/springcloud-cunsumer-dept-80/src/main/java/com/springcloud/study/config/configBean.java
@@ -1,5 +1,7 @@
 package com.springcloud.study.config;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,13 +10,25 @@ import org.springframework.web.client.RestTemplate;
 @Configuration //原先是从spring的xml注册bean,现在可以用配置类 -- spring applicationContext.xml
 public class configBean {
 
-    @Bean
     //配置负载均衡实现RestTemplate
-    @LoadBalanced //Ribbon
+    //开启负载均衡
+    @LoadBalanced
+    @Bean
     public RestTemplate getRestTemplate(){
-
         return new RestTemplate();
     }
+
+
+    /**
+     * 自定义的ribbon策略配置不能放在@ComponentScan所扫描的当前包下以及子包下，
+     * 否则我们自定义的这个配置类就会被所有的Ribbon客户端所共享。通过查看源代码来看，启动类一般加@SpringBootApplication，
+     * 而它底封装了@ComponentScan注解，所以自定义的代码文件不能和启动类放在同一个包下
+     */
+//    @Bean
+//    public IRule myRule(){
+//
+//        return new RandomRule(); //随机
+//    }
 
 
 }
